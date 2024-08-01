d7d3222ae5b8d2877d4d96e4c0bef58d15b9b3e9
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-08-01 12:53:59 -0300

    DepartmentList view design

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module org.example.workshopjavafxjdbc {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
+    requires jdk.compiler;
 
     opens org.example.workshopjavafxjdbc to javafx.fxml;
     opens org.example.workshopjavafxjdbc.gui to javafx.fxml;
--- src/main/java/org/example/workshopjavafxjdbc/gui/MainViewController.java
+++ src/main/java/org/example/workshopjavafxjdbc/gui/MainViewController.java
@@ -1,9 +1,19 @@
 package org.example.workshopjavafxjdbc.gui;
 
+import com.sun.tools.javac.Main;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import org.example.workshopjavafxjdbc.HelloApplication;
+import org.example.workshopjavafxjdbc.gui.util.Alerts;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -23,16 +33,34 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemDepartmentAction() {
-        System.out.println("onMenuItemDepartmentAction");
+        loadView("/org/example/workshopjavafxjdbc/gui/DepartmentList.fxml");
     }
 
     @FXML
     public void onMenuItemAboutAction() {
-        System.out.println("onMenuItemAboutAction");
+        loadView("/org/example/workshopjavafxjdbc/gui/About.fxml");
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    private void loadView(String absoluteName) {
+       try {
+           FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+           VBox newVbox = loader.load();
+
+           Scene mainScene = HelloApplication.getMainScene();
+           VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+           Node mainMenu = mainVbox.getChildren().get(0);
+           mainVbox.getChildren().clear();
+           mainVbox.getChildren().add(mainMenu);
+           mainVbox.getChildren().addAll(newVbox.getChildren());
+
+       } catch (IOException e) {
+           Alerts.showAlert("Io Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+       }
     }
 }
